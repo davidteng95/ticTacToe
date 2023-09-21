59278631561e65828b7df8a29effc040a49fb11a
davidteng95 <davidteng95@gmail.com>
2023-09-21 17:08:29 -0600

    implement human marking and replay button

---
+++ script.js
@@ -0,0 +1,36 @@
+var originalBoard;
+let humanPlayer = "O";
+let aiPlayer = "X";
+
+const winCombos = [
+  [0 ,1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
+
+const cells = document.querySelectorAll('.cell');
+startGame();
+
+function startGame() {
+  document.querySelector(".endgame").style.display = "none"
+  originalBoard = Array.from(Array(9).keys());
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = '';
+    cells[i].style.removeProperty('background-color');
+    cells[i].addEventListener('click', turnClick, false);
+  }
+}
+
+function turnClick(square) {
+  turn(square.target.id, humanPlayer);
+}
+
+function turn(squareId, player) {
+	originalBoard[squareId] = player;
+	document.getElementById(squareId).innerText = player;
+}
